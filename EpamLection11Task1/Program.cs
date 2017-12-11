7dc17f2ba62c65442b1fd1fbe58bf36f028054c9
Illya Dekhtyarenko <rokaf3@gmail.com>
2017-12-11 20:29:01 +0200

    Добавьте файлы проекта.

---
+++ EpamLection11Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamLection11Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(1, 1, 1);
+            Vector vector2 = new Vector(2, 2, 2);
+
+            //Show Vectors
+            vector1.Output();
+            vector2.Output();
+            
+            //Substraction of Vectors
+            Vector.ShowSubstraction(vector1,vector2);
+
+            //Addition of Vectors
+            Vector.ShowAddition(vector1, vector2);
+            
+            //If vectors are Equal
+            vector1.IfEquals(vector2);
+
+            //Length of vector
+            vector1.ShowLength();
+
+            //Vector pruduct of Vectors
+            Vector.ShowMulti(vector1, vector2);
+
+            //Scalar product of Vectors
+            Vector.ShowScalarMulti(vector1, vector2);
+
+            //Mix product of Vectors
+            Vector vector3 = new Vector(3, 3, 3);
+            Vector.ShowMixMulti(vector1, vector2, vector3);
+
+            //Angle of Vectors
+            Vector.ShowAngle(vector1,vector2);
+         
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EpamLection11Task1/Vector.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamLection11Task1
+{
+    public class Vector
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+
+        public Vector(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Vector {this.x} {this.y} {this.z}");
+        }
+
+
+        public double Length()
+        {
+            return Math.Sqrt((int)(x * x) + (int)(y * y) + (int)(z * z));
+        }
+
+        public static Vector Multi(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.y * vector2.z - vector1.z * vector2.y, vector1.z * vector2.x - vector1.x * vector2.z, vector1.x * vector2.y - vector1.y * vector2.x);
+        }
+
+
+        public static int MixMulti(Vector vector1, Vector vector2, Vector vector3)
+        {
+            return (vector1.x * vector2.y * vector3.z) + (vector1.y * vector2.z * vector3.x) + (vector1.z * vector2.x * vector3.y) - (vector1.z * vector2.y * vector3.x) - (vector1.y * vector2.x * vector3.z) - (vector1.x * vector2.z * vector3.y);
+        }
+
+
+
+        public static int ScalarMulti(Vector vector1, Vector vector2)
+        {
+            return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
+        }
+
+
+        public static double Angle(Vector vector1, Vector vector2)
+        {
+            return ScalarMulti(vector1, vector2) / (vector1.Length() * vector2.Length());
+        }
+
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
+        }
+
+
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            return new Vector(vector1.x - vector2.x, vector1.y - vector2.y, vector1.z - vector2.z);
+        }
+
+
+        public bool Equals(Vector vector1)
+        {
+            if (this.x == vector1.x && this.y == vector1.y && this.z == vector1.z) return true;
+            else return false;
+        }
+
+        public void IfEquals(Vector vector1)
+        {
+            if (Equals(vector1)) Console.WriteLine("Vectors are equal");
+            else Console.WriteLine("Vectors aren't equal");
+        }
+
+        public static void ShowAddition(Vector vector1, Vector vector2)
+        {
+            Console.Write("Addition of vectors is ");
+            (vector1 + vector2).Output();
+        }
+
+        public static void ShowSubstraction(Vector vector1, Vector vector2)
+        {
+            Console.Write("Substraction of vectors is ");
+            (vector1 - vector2).Output();
+        }
+
+        public void ShowLength()
+        {
+            Console.WriteLine("Vector length is {0}", Length());
+        }
+
+        public static void ShowMulti(Vector vector1, Vector vector2)
+        {
+            Console.Write("Result of vector product is ");
+            (Multi(vector1, vector2)).Output();
+        }
+
+        public static void ShowMixMulti(Vector vector1, Vector vector2, Vector vector3)
+        {
+            Console.WriteLine("Mixed product of vectors is {0}", MixMulti(vector1, vector2, vector3));
+        }
+
+        public static void ShowScalarMulti(Vector vector1, Vector vector2)
+        {
+            Console.WriteLine("Scalar product of vectors is {0}", ScalarMulti(vector1, vector2));
+        }
+
+        public static void ShowAngle(Vector vector1,Vector vector2)
+        {
+            Console.WriteLine("Angle of vectors is {0}", Angle(vector1, vector2));
+        }
+    }
+}
